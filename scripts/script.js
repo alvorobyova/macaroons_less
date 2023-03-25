d34c1b35b0121723fe2952db2cdd3fe40bfefcd3
Alena <112640274+alvorobyova@users.noreply.github.com>
2023-03-25 09:41:48 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,12 @@ $(document).ready(function () {
     let buttonSubmit = $('#submit');
     let form = $('#form');
 
+    // добавление названия макаруна в форму
+    $('.order__btn').click(event => {
+        $('.order')[0].scrollIntoView({behaviour: 'smooth'});
+        $('#product').val($(event.target).parents('.macaroon').find('.macaroon__title').text());
+    })
+
     buttonSubmit.click(function () {
 
         let name = $('#name');
@@ -29,7 +35,7 @@ $(document).ready(function () {
         let orderInfoElements = $('.order__info');
         let orderWindowSuccess = $('.order__successfull');
         let orderImage = $('.order__image');
-        loader.css('display', 'flex');
+
 
         $('.order__error').hide();
         input.css('borderColor', '#493135');
@@ -50,28 +56,31 @@ $(document).ready(function () {
             hasError = true;
         }
 
-        if (hasError) {
+        if (!hasError) {
+            loader.css('display', 'flex');
 
             $.ajax({
                 method: "POST",
-                url: "http://testologia.site/checkout",
+                url: "https://testologia.site/checkout",
                 data: {product: product.val(), name: name.val(), phone: phone.val()}
             })
                 .done(function (msg) {
                     loader.css('display', 'none');
                     // console.log(msg);
-                    if (msg.success === 1 && msg.name === 'itlogia') {
-                        orderInfoElements.attr('visibility', 'hidden');
-                        orderWindowSuccess.attr('visibility', 'visible');
-                        orderImage.attr('opacity', '0.3');
-                    }
-                    /*если не добавлять в условие тот факт, что значения в полях есть, то алерт будет появляться,
-                    даже когда они не заполнены, что противоречит условиям ТЗ*/
-                    else if (product.val() && name.val() && phone.val() && msg.success === 0) {
+
+                    if (msg.success === 1) {
+                        orderInfoElements.hide();
+                        orderWindowSuccess.css('visibility', 'visible');
+                        orderImage.hide();
+
+                        product.val('');
+                        name.val('');
+                        phone.val('');
+                    } else {
                         alert('Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ.')
                     }
                 });
         }
-    })
+    });
 
 })
